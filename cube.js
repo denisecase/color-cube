5deb5bed0ca707da25e912df4da366383ddcbbe8
brucehjohnson <127142432+brucehjohnson@users.noreply.github.com>
2023-12-28 14:08:21 -0600

    2*(2*Math.PI/6)

--- cube.js
+++ cube.js
@@ -50,7 +50,7 @@ function init() {
 //  cubeGroup.rotateOnAxis(axis, angle);
 
   let axis = new THREE.Vector3(1, 0, -1).normalize();
-  let angle = 2*Math.PI/6;
+  let angle = 2*(2*Math.PI/6);
   cubeGroup.rotateOnAxis(axis, angle);  
   // END ALIGN ***************************
 
