4689e4f7a3129180512034868f33351de8ec7f65
Bruce Johnson <brucehjohnson@comcast.net>
2024-01-01 13:42:26 -0600

    slowed the spin

--- js/ui.js
+++ js/ui.js
@@ -12,18 +12,18 @@ import {
 } from '../cube.js';
 
 export const inputs = {
-  doAnimateGap: true,  // BHJ: change to false while working
+  doAnimateGap: false,  // BHJ: change to false while working
   doAnimateRotation: true, // BHJ: change to false while working
   rotation: 0,
 };
 
 export const cubeSettings = {
   size: 1,
-  gap: 0.4,
+  gap: 0.2,
   gapChangeDirection: 1,
   maxGap: 1.0,
   minGap: 0.3,
-  rotationSpeed: 0.01,
+ // rotationSpeed: 0.002,
 };
 
 //#region ROTATION ANIMATION AND SELECTION SLIDER
@@ -48,24 +48,44 @@ rotationCheckbox.addEventListener('change', (event) => {
   animateOrRender();
 });
 
-rotationSlider.addEventListener('input', () => {
+//  ROTATION SLIDER GETS TWO EVENTS
+
+  rotationSlider.addEventListener('input', () => {
   // If the user changes the rotation slider, update the rotation value
   // Make sure the rotation animation is off
+
   const rotation = parseInt(rotationSlider.value);
   document.getElementById('rotation').textContent = rotation;
-  setUserRotation(rotation);
+ // setUserRotation(rotation);
+
+  // always call this at the end
   animateOrRender();
 });
 
-setRotationSlider.addEventListener('click', () => {
+setRotationSlider.addEventListener('click', (event) => {
   // If the user clicks on the set rotation slider, stop the animation
   // and set the rotation to the current value of the slider
   inputs.doAnimateRotation = false;
   rotationCheckbox.checked = inputs.doAnimateRotation;
+
+  // BHJ: Add if the value has NOT changed, then do the folloowing
+  // also rotate a bit in the direction they provided
+
+  if (rotationSlider.value == rotationSlider.value) {
+  const rotation = parseInt(rotationSlider.value);
+  document.getElementById('rotation').textContent = rotation;
+  +setUserRotation(rotation);
+
+}
+
+  // always call this at the end
+
   animateOrRender();
 });
 
 // Rotation-related functions
+
+//const rotationAngle = 0.004; // radians per frame
 
 /**
  * This function is called continuously to animate the rotation of the cube group.
@@ -73,8 +93,9 @@ setRotationSlider.addEventListener('click', () => {
 export function animateRotation() {
   if (inputs.doAnimateRotation) {
     const rotationVector = getNew3DVector(0, 0, 1);
-    const rotationSpeed = 0.01; // radians per frame
-    cubeGroup.rotateOnWorldAxis(rotationVector, rotationSpeed);
+    // const rotationAngle = 0.004; // radians per frame
+   // cubeGroup.rotateOnWorldAxis(rotationVector, rotationAngle);
+     cubeGroup.rotateOnWorldAxis(rotationVector, 0.004);
   }
 }
 
@@ -83,15 +104,16 @@ export function animateRotation() {
  * @param {
  * } inputRotation 
  */
+
 export function setUserRotation(inputRotation) {
   inputs.rotation = inputRotation; // Update the rotation value
   const rotationVector = getNew3DVector(0, 0, 1);
-  const rotationSpeed = 0.01; // radians per frame
 
-  const amount1 = (inputs.rotation * Math.PI) / 180;
-  const amount2 = 0.01;
-  const amount3 = inputs.rotation + 10;
-  cubeGroup.rotateOnWorldAxis(rotationVector, cubeSettings.rotationSpeed);
+ let axis = rotationVector;
+ let angle = (2*Math.PI/360)*inputs.rotation;
+
+  cubeGroup.rotateOnWorldAxis(axis, angle);
+
 }
 
 //#endregion ROTATION ANIMATION AND SELECTION SLIDER
