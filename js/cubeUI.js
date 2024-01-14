d5419a70e5554024c80ef7728030c2bb939cc9b4
Denise Case <denisecase@gmail.com>
2024-01-14 11:10:25 -0600

    expand

--- js/cubeUI.js
+++ js/cubeUI.js
@@ -38,7 +38,7 @@ export const cubeSettings = {
   size: 1,
   cubeCount: 9,
   colorList: [0, 32, 64, 96, 128, 160, 192, 224, 255],
-  initialGap: 0.2,
+  initialGap: 0.5,
 };
 
 const cubeCountControls = document.querySelectorAll('input[name="cubeCount"]');
